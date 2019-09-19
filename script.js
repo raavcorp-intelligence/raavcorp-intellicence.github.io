b5886d380855056a6a7d08a74cbd692d075e6f0a
Ryan Vogel <ryan.vogel2004@gmail.com>
2019-09-19 06:56:21 -0400

    Rosii

---
+++ script.js
@@ -0,0 +1,9 @@
+function x1() {
+  var person = prompt("Please enter your name", "");
+  if (person != "") {
+    alert("Hi " + person + "! I am so glad you clicked on this button to learn about")
+    }
+  else {
+        alert("You didn't enter a name!");
+    }
+}
